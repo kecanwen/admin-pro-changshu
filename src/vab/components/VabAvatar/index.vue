4907c042cb60a503888aa174fd5bf177be9c0b2d
kecanwen <kcw199611252815@163.com>
2023-08-27 23:32:45 +0800

    修改密码优化

--- src/vab/components/VabAvatar/index.vue
+++ src/vab/components/VabAvatar/index.vue
@@ -22,11 +22,60 @@
           {{ translateTitle('退出登录') }}
         </el-dropdown-item>
         <el-dropdown-item command="fixup">
-          <vab-icon icon="el-icon-edit" />
+          <i class="el-icon-edit"></i>
           {{ translateTitle('修改密码') }}
         </el-dropdown-item>
       </el-dropdown-menu>
     </template>
+    <el-dialog
+      append-to-body
+      :before-close="handleClose"
+      title="修改密码"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
+      <el-form
+        ref="registerForm"
+        class="register-form"
+        :model="form"
+        :rules="registerRules"
+        size="mini"
+      >
+        <el-form-item prop="password">
+          <el-input
+            v-model.trim="form.password"
+            autocomplete="new-password"
+            :placeholder="translateTitle('请输入新密码')"
+            type="password"
+          >
+            <template #prefix>
+              <vab-icon icon="lock-line" />
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item prop="password1">
+          <el-input
+            v-model.trim="form.password1"
+            autocomplete="new-password"
+            :placeholder="translateTitle('请确认密码')"
+            type="password"
+          >
+            <template #prefix>
+              <vab-icon icon="lock-line" />
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            class="register-btn"
+            type="primary"
+            @click.native.prevent="handleRegister"
+          >
+            {{ translateTitle('确认') }}
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </el-dropdown>
 </template>
 
@@ -34,12 +83,50 @@
   import { translateTitle } from '@/utils/i18n'
   import { mapActions, mapGetters } from 'vuex'
   import { toLoginRoute } from '@/utils/routes'
+  import { fixupPassword } from '@/api/user'
+  import { isPassword } from '@/utils/validate'
 
   export default {
     name: 'VabAvatar',
     data() {
+      const validatePassword = (rule, value, callback) => {
+        if (!isPassword(value)) {
+          callback(new Error(this.translateTitle('密码不能少于6位')))
+        } else {
+          callback()
+        }
+      }
+      const validatePassword1 = (rule, value, callback) => {
+        if (!isPassword(value)) {
+          callback(new Error(this.translateTitle('密码不能少于6位')))
+        } else if (this.form.password !== this.form.password1) {
+          callback(new Error(this.translateTitle('两次输入密码不一致')))
+        } else {
+          callback()
+        }
+      }
       return {
         active: false,
+        dialogVisible: false,
+        form: {},
+        registerRules: {
+          password: [
+            {
+              required: true,
+              trigger: 'blur',
+              message: this.translateTitle('请输入密码'),
+            },
+            { validator: validatePassword, trigger: 'blur' },
+          ],
+          password1: [
+            {
+              required: true,
+              trigger: 'blur',
+              message: this.translateTitle('请确认密码'),
+            },
+            { validator: validatePassword1, trigger: 'blur' },
+          ],
+        },
       }
     },
     computed: {
@@ -53,6 +140,15 @@
       ...mapActions({
         _logout: 'user/logout',
       }),
+      handleClose() {},
+      handleRegister() {
+        this.$refs['registerForm'].validate(async (valid) => {
+          if (valid) {
+            const { msg } = await fixupPassword(this.form)
+            this.$message.info(msg)
+          }
+        })
+      },
       handleCommand(command) {
         switch (command) {
           case 'logout':
@@ -62,7 +158,7 @@
             this.personalCenter()
             break
           case 'fixup':
-            this.fixup()
+            this.dialogVisible = true
             break
         }
       },
@@ -71,9 +167,6 @@
       },
       personalCenter() {
         this.$router.push('/setting/personalCenter')
-      },
-      fixup() {
-        this.$router.push('/fixupPassword')
       },
       async logout() {
         await this._logout()
